23f41c53056232271ebc874f38976f242d5d6934
Soyeon Jang <ssso.pro1@gmail.com>
2022-02-14 17:27:21 +0900

    feat : add sphere, lights, helpers, animate

--- main.js
+++ main.js
@@ -1,6 +1,66 @@
-import './style.css'
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// Setup  ------------------------------------------------------
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// Sphere  ------------------------------------------------------
+
+const geometry = new THREE.SphereGeometry(15, 32, 16);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const sphere = new THREE.Mesh(geometry, material);
+
+scene.add(sphere);
+
+// Lights ------------------------------------------------------
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// Helpers
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Functions -------------------------------------------------------
+
+// animate
+function animate() {
+  requestAnimationFrame(animate);
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
+  sphere.rotation.z += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
