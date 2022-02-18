9f37ff55c9175d0eb942b2867840d5d050ff74fe
Soyeon Jang <ssso.pro1@gmail.com>
2022-02-18 16:23:39 +0900

    fixing : working on same error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   base: '/dist/',
   build: {
     chunkSizeWarningLimit: 1500,
@@ -15,4 +15,4 @@ export default defineConfig({
       },
     },
   },
-});
+};
